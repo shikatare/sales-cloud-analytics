9c9e4a46bbe628e5709d595c82b9a816a1886e7c
learning- <anshika.katare@gmail.com>
2026-03-11 13:59:24 +0530

    Added eda for dataset

---
+++ scripts/eda_analysis.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+df=pd.read_csv('scripts/dataclean/orders_cleaned.csv')
+print("Cleaned Data Shape:",df.shape)
+print(df.columns)
+sales_category=df.groupby('Category')['Sales'].sum()
+print("\n Sales by Category:\n",sales_category)
+sales_category.plot(kind='bar',title='Total Sales by Category')
+plt.xlabel('Category')
+plt.ylabel('Total Sales')
+plt.show()
+sales_region=df.groupby('Region')['Sales']/sum()
+print("\n Sales by Region:\n",sales_region)
+sales.region.plot(kind="bar",title='Sales Distribution by Region')
+plt.show()
